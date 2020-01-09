c599df2779dc2872681ddf49ef0b83a8ac646740
Aaron <hello@aaronfagan.ca>
2020-01-09 13:32:00 -0700

    feat: basic sponsor page ready

--- assets/js/www.js
+++ assets/js/www.js
@@ -2,7 +2,6 @@
 // BEGIN DOCUMENT READY /////////////////////////////////////////////////////
 $(document).ready(function() {
 /*---------------------------------------------------------------------------------------------*/
-
 	$('button.stripe').click(function(event) {
 		var str = window.location.href;
 		var domain = str.substring(0, str.lastIndexOf('/'));
@@ -16,22 +15,16 @@ $(document).ready(function() {
 			cancelUrl: domain + '/canceled.html?session_id={CHECKOUT_SESSION_ID}'
 		}).then(handleResult);
 	});
-
-
 	var handleResult = function(result) {
 		if (result.error) {
 			var displayError = document.getElementById('error-message');
 			displayError.textContent = result.error.message;
 		}
 	};
-
 	var urlParams = new URLSearchParams(window.location.search);
-
 	if (urlParams.has('session_id')) {
 		document.getElementById('session').textContent = urlParams.get('session_id');
 	}
-
-
 /*---------------------------------------------------------------------------------------------*/
 // END DOCUMENT READY
 });
