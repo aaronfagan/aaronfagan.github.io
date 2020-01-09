4832d3b142b07683a60cd8c696b7221e4c9cbf4e
Aaron <hello@aaronfagan.ca>
2020-01-09 13:30:49 -0700

    feat: basic sponsor page ready

--- assets/js/www.js
+++ assets/js/www.js
@@ -4,36 +4,31 @@ $(document).ready(function() {
 /*---------------------------------------------------------------------------------------------*/
 
 	$('button.stripe').click(function(event) {
-		var domain = window.location.href.substring(0, str.lastIndexOf("/"));
+		var str = window.location.href;
+		var domain = str.substring(0, str.lastIndexOf('/'));
 		alert(domain);
-	 	var stripeSku = $(this).attr("data-stripe-sku");
-	    var stripePlan = $(this).attr("data-stripe-plan");
+	 	var stripeSku = $(this).attr('data-stripe-sku');
+	    var stripePlan = $(this).attr('data-stripe-plan');
 	    var items = stripeSku ? [{ sku: stripeSku, quantity: 1 }] : [{ plan: stripePlan, quantity: 1 }];
-	    stripe
-	      .redirectToCheckout({
-	        items: items,
-	        successUrl:
-	          domain + "/success.html?session_id={CHECKOUT_SESSION_ID}",
-	        cancelUrl:
-	          domain + "/canceled.html?session_id={CHECKOUT_SESSION_ID}"
-	      })
-	      .then(handleResult);
+	    stripe.redirectToCheckout({
+			items: items,
+			successUrl: domain + '/success.html?session_id={CHECKOUT_SESSION_ID}',
+			cancelUrl: domain + '/canceled.html?session_id={CHECKOUT_SESSION_ID}'
+		}).then(handleResult);
 	});
 
 
 	var handleResult = function(result) {
-	  if (result.error) {
-	    var displayError = document.getElementById("error-message");
-	    displayError.textContent = result.error.message;
-	  }
+		if (result.error) {
+			var displayError = document.getElementById('error-message');
+			displayError.textContent = result.error.message;
+		}
 	};
 
 	var urlParams = new URLSearchParams(window.location.search);
 
-	if (urlParams.has("session_id")) {
-	  document.getElementById("session").textContent = urlParams.get(
-	    "session_id"
-	  );
+	if (urlParams.has('session_id')) {
+		document.getElementById('session').textContent = urlParams.get('session_id');
 	}
 
 
